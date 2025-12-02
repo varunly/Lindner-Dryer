0a100b7a04368af1d0d5433297703aecbeff14bb
Varun Solanki <63137256+varunly@users.noreply.github.com>
2025-12-02 10:33:03 +0100

    Update dryer_kpi_monthly_final.py

--- dryer_kpi_monthly_final.py
+++ dryer_kpi_monthly_final.py
@@ -21,7 +21,6 @@ CONFIG = {
     "gas_to_kwh": 11.5,
     "zones_seq": ["Z1", "Z2", "Z3", "Z4", "Z5"],
     "num_thermal_zones": 4,
-    "PLATES_PER_WAGON": 234,
 }
 
 ZONE_ENERGY_MAPPING = {
@@ -32,6 +31,7 @@ ZONE_ENERGY_MAPPING = {
 }
 
 SUSPENSION_KG = 330
+PLATES_PER_WAGON = 234  # Standard number of plates per wagon
 
 PRODUCT_SPECIFICATIONS = {
     "L28": {
@@ -735,6 +735,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
